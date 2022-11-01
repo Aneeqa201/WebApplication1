15aee464fc4d8f22b589f26506fb808ad1bb7593
Aneeqa201 <116572452+Aneeqa201@users.noreply.github.com>
2022-11-01 18:39:34 +0500

    Add files via upload

---
+++ WebApplication1/JsonPatientFile.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public class JsonPatientFile
+    {
+
+        // path -from  
+        public JsonPatientFile(IWebHostEnvironment webHostEnvironment)
+        {
+             WebHostEnvironment = webHostEnvironment;
+        }
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        public string JsonFilePath 
+        {
+
+            get
+            {
+                return Path.Combine(WebHostEnvironment.WebRootPath, "data", "patients.json");
+            }
+        }
+
+
+        public IEnumerable<patient> getpatientRecords()
+        {
+            using (var file_stream = File.OpenText(JsonFilePath)) 
+            {
+                return JsonSerializer.Deserialize<patient[]>(file_stream.ReadToEnd());
+            }
+
+        }
+    }
+}
---
+++ WebApplication1/patient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace WebApplication1.Models
+{
+    public class patient
+    {
+        [JsonPropertyName("id")]
+        public int patient_id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string patient_name { get; set; }
+
+        [JsonPropertyName("image")]
+        public string patient_image { get; set; }
+
+
+        //convert all data into string format (json string)
+
+        public override string ToString()
+        {
+
+            //  return base.ToString(); by default
+            return JsonSerializer.Serialize<patient>(this);   // for converting into json format string
+        }
+    }
+}
+
